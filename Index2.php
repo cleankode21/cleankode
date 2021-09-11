65ce9edda59838fa22ba88d295cca813111d890a
CleanKode <88700860+cleankode21@users.noreply.github.com>
2021-09-11 10:47:24 -0500

    Add files via upload
    Home page with effects 
    before the new interface

---
+++ Index2.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="google" value="notranslate"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="js/Apps.js"></script>
+    <link rel="shortcut icon" href="src/loga.png" />
+    <title>CleanKode</title>
+    <link rel="stylesheet" href="css/Styles2.css">
+</head>
+
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark"  id="navbar">
+<img src="src/loga.png" width="60" height="60" class="d-inline-block align-top" alt="">
+<a class="navbar-brand" href="#">CleanKode</a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Servicios</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Proyectos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nosotros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><img src="src/idioma-en-ingles.png" width="30" height="30" class="d-inline-block align-top" alt=""></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<!-- HEADER -->
+<div class="header">  
+
+<div class="inner-header flex">
+<h1>¿Necesecitas una Pagina Web?
+  <p></p>
+  <p></p>
+  <p>Somos creadores de páginas web y aplicaciones móviles. Para comercios, Empresas, Personales, Etc.</p>
+  <p>Nos diferenciamos de la competencia ya que no importa el proyecto, te acompañamos paso a paso hasta el final. Además, que nos consideramos flexibles a tus posibilidades.</p></h1>
+ 
+</div>
+<button type="button" class="btn btn-success"><p>Realiza tu Cotización</p></button> 
+
+<div>
+<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+<defs>
+<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+</defs>
+<g class="parallax">
+<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+</g>
+</svg>
+</div>
+
+</div>
+
+<body >
+          <div class="title">
+            <h2 class="font-weight-bold">Desarrollamos tu Pagina Web a la Medida</h2>
+            <p class="Subtitle font-weight-normal">¿Que sitios web podemos realizar?</p>
+          </div>
+          <!-- Tienda en linea -->
+        <div class="contenido " >
+            <div class="imgref ">
+                <img src="src/ecommerce.png" alt="imagen de ecommerce">
+            </div>
+            <div class="descripcion">
+                <div class="Tservicio">
+                    <h3>E-Commerce o Tienda en linea</h3>
+                </div>
+                <div class="Iservicio">
+                    <p>¡llega a más clientes! No solo de tu ciudad si no de varios lugares en el mundo y aumenta tus ventas. Tenemos varias opciones para tí</p>
+                </div>
+            </div>
+        </div>
+        <!--  pagina basica -->
+        <div class="contenido2"> 
+            <div class="descripcion">
+                <div class="Tservicio">
+                    <h3>Pagina Web Basica</h3>
+                </div>
+                <div class="Iservicio">
+                    <p>Sí solo quieres darte a conocer una pagina basica es lo ideal. Ya que mostraremos lo mejor de tu empresa para darte a conocer</p>
+                </div>     
+            </div>
+            <div class="imgref">
+                <img src="src/estatica.png" alt="imagen basica">
+            </div>
+        </div>
+         <!-- Portafolio pagina personal -->
+         <div class="contenido">
+            <div class="imgref">
+                <img src="src/portafolio.png" alt="imagen de portafolio">
+            </div>
+            <div class="descripcion">
+                <div class="Tservicio">
+                    <h3>Portafolio o Pagina personal</h3>
+                </div>
+                <div class="Iservicio">
+                    <p>¿Eres un profecional? y quieres dar a conocer tus Proyectos o trabajos,nada mejor que un portafolio profecional ¡Tú propio sito web personal!</p>
+                </div>       
+            </div>   
+        </div>
+        <!-- paginas responsivas -->
+        <div class="contenido2"> 
+            <div class="descripcion">
+                <div class="Tservicio">
+                    <h3>Sitios Web Responsivos</h3>
+                </div>
+                <div class="Iservicio">
+                    <p>Desarrolamos sitios web completamente adapatables a todos los dispositivos como celulares, tablet´s o comuptadoras de escritorio</p>
+                </div>     
+            </div>
+            <div class="imgref">
+                <img src="src/responsivo.png" alt="imagen basica">
+            </div>
+        </div>
+        <div class="btn-cotizacion">
+        <button type="button" class="btn btn-success btn-cotizacion"><p>Más información</p></button> 
+        </div>
+
+        <!-- seccion de apps -->
+        <!-- fondo -->
+        <div class="fondo">
+            <div class="ondaleft">
+                <img src="src/wave-1.png" alt="ondas">
+            </div>
+            <div class="central">
+
+            </div>
+            <div class="ondaderecha">
+                <img src="src/wave-3.png" alt="ondas">
+            </div>
+             <!-- contenido fondo -->
+         <div class="content">
+            <div class="apptitulo">
+                <h2>Diseño y Creación de Aplicaciones móviles multiplataforma</h2>
+            </div>
+            <div class="contenido-apps">
+                <div class="cards"> <!--Card de apple-->
+                    <div class="card apple" >
+                        <div class="logoservicio">
+                            <img src="src/apple.png" alt="">
+                        </div>
+                        <div class="des">
+                            <div class="titulo-card">
+                                <h1>Desarrollo de Apps IOS</h1>
+                            </div>
+                            <div class="info-card">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quod vitae ab asperiores obcaecati consectetur libero excepturi impedit maxime, necessitatibus veniam labore quisquam blanditiis quia nesciunt ullam sunt maiores officiis.</p>
+                            </div>
+                        </div>
+                    </div>
+                 <!--Fin de card de apple-->
+                 <!--Card de android-->
+                 <div class="card android" >
+                        <div class="logoservicio">
+                            <img src="src/play.png" alt="">
+                        </div>
+                        <div class="des">
+                            <div class="titulo-card">
+                                <h1>Desarrollo de Apps Android</h1>
+                            </div>
+                            <div class="info-card">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quod vitae ab asperiores obcaecati consectetur libero excepturi impedit maxime, necessitatibus veniam labore quisquam blanditiis quia nesciunt ullam sunt maiores officiis.</p>
+                            </div>
+                        </div>
+                    </div>
+                 <!--Fin de card de android-->
+
+                <!--Card de nativas-->
+                    <div class="card nativas" >
+                        <div class="logoservicio">
+                            <img src="src/nativa.png" alt="">
+                        </div>
+                        <div class="des">
+                            <div class="titulo-card">
+                                <h1>Desarrollo de Apps Nativas multiplataforma</h1>
+                            </div>
+                            <div class="info-card">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quod vitae ab asperiores obcaecati consectetur libero excepturi impedit maxime, necessitatibus veniam labore quisquam blanditiis quia nesciunt ullam sunt maiores officiis.</p>
+                            </div>
+                        </div>
+                    </div>
+                 <!--Fin de card de nativas-->
+                </div>
+                <div class="cel">
+                    <img src="src/cel.png" alt="celular">
+                </div>
+                
+            </div>
+            <div class="btn-cotizacion apps">
+        <button type="button" class="btn btn-success btn-cotizacion"><p>Más información</p></button> 
+            </div>
+        </div>
+         </div>
+         <div class="contenido-soft">
+             <div class="fila1">
+                <div class="tarjeta amarilla">
+                    <div class="imgtarjeta">
+                        <img src="src/puntodeventa.png" alt="imagen punto de venta">
+                    </div>
+                    <div class="infotar">
+                    <h3>Puntos de Venta</h3>
+                    <hr>
+                    <p>LLeva el control de tu mercancía y ganancias con un administrador como lo es un punto de venta </p>
+                    </div>
+                </div> <!--tarjeta punto de venta -->
+                <div class="tarjeta morado">
+                    <div class="imgtarjeta">
+                        <img src="src/administracion.png" alt="imagen punto de venta">
+                    </div>
+                    <div class="infotar">
+                    <h3>Gestion de empleados o alumnos </h3>
+                    <hr>
+                    <p>Lleva el control de tu empresa con un gestor de empleados o alumnos, tenemos varias opciones  </p>
+                    </div>
+                </div> <!--tarjeta punto de administrador -->
+             </div>
+             <div class="fila2">
+                <div class="tarjeta azul">
+                    <div class="imgtarjeta">
+                        <img src="src/creacion.png" alt="imagen punto de venta">
+                    </div>
+                    <div class="infotar">
+                    <h3>¿No sabes si necesita un sistema o aplicación?</h3>
+                    <hr>
+                    <p>No te preocupes, cuéntanos tu día a día en tu empresa y nuestros analistas te dirán que puedes sistematizar</p>
+                    </div>
+                </div> <!--tarjeta punto de software libre -->
+
+        
+             </div>
+             
+             <div class="btn-cotizacion contactanos">
+        <button type="button" class="btn btn-success btn-cotizacion"><p>Contactanos para más información</p></button> 
+               
+            </div>
+         </div>
+         <div class="inner-header flex">
+<h1>¿Necesecitas una Pagina Web?
+  <p></p>
+  <p></p>
+  <p>Somos creadores de páginas web y aplicaciones móviles. Para comercios, Empresas, Personales, Etc.</p>
+  <p>Nos diferenciamos de la competencia ya que no importa el proyecto, te acompañamos paso a paso hasta el final. Además, que nos consideramos flexibles a tus posibilidades.</p></h1>
+ 
+</div>
+<button type="button" class="btn btn-success"><p>Realiza tu Cotización</p></button> 
+
+<div class="footerSite">  
+<div>
+<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+<defs>
+<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+</defs>
+<g class="parallax">
+<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(17,12,193,1)" />
+<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(17,12,193,0.5)" />
+<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(17,12,193,0.3)" />
+<use xlink:href="#gentle-wave" x="48" y="7" fill="#110CC1" />
+</g>
+</svg>
+</div>
+</div>
+
+<footer class="footer">
+        <div class="footer1">
+            <div class="seccion">
+                <div class="logo2">
+                    <img src="src/loga-1.png" alt="">
+                    <p>CleanKode.</p>
+                </div>
+            </div>
+            <div class="seccion">
+            <div class="infoclean">
+                <div class="clean">
+                    <p>Desarrolladores de Paginas web, App´s móviles y Software dedicado 
+                    </p>
+                </div>
+        
+                <div class="ciudad">
+                    <p>
+                        Ciudad de México, México.
+
+                    </p>
+                </div>
+                </div>
+            </div>
+            <div class="seccion">
+                <div class="infoservicios">
+                    <p>Servicios</p>
+                    <ul>
+                        <li>Desarrollo de paginas web</li>
+                        <li>Desarrollo de App´s móviles</li>
+                        <li>Desarrollo de Software dedicado</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="seccion">
+                    <div class="contacto-footer">
+                    <h4>Contacto</h4>
+                    <p>cleankode21@gmail.com</p>
+                    <p>5511208028</p>
+                </div>
+                    <div class="redes">
+                        <div class="red"><a href="https://www.facebook.com/CleanKode"><img src="src/facebook.png" alt="facebook"></a></div>
+                        <div class="red"><a href="https://www.instagram.com/clean_kode/"><img src="src/instagram.png" width="45" height="45"  alt="instagram"></a></div>
+                        <div class="red"><a href="https://www.youtube.com/channel/UCownV9NSPHs0cKgmMiek0CQ"><img src="src/youtube.png" alt="youtube"></a></div>
+                        <div class="red"><a href="https://twitter.com/CleanKode"><img src="src/tweeter.png" alt="tweeter"></a></div>
+                        <div class="red"><a href=""><img src="src/whatsapp.png" alt="whatsapp"></a></div>
+                </div>
+            </div>
+        </div>
+        <div class="footer2">
+            <p>Copyright © 2021 CleanKode | Todos los derechos reservados - Política de Privacidad, Aviso Legal y Cookies - Condiciones generales</p>
+        </div>
+    </footer>
+</div>
+</div>
+</body>
+</html>
